8eb3483489f8c525dc04de48cd6e2636879f65e7
Kyle Jake Plimack <kyle.plimack@gmail.com>
2012-10-03 19:10:32 -0400

    separate index into sectioned pages

--- wiki/admin.py
+++ wiki/admin.py
@@ -3,3 +3,4 @@ from django.contrib import admin
 
 admin.site.register(Author)
 admin.site.register(Page)
+admin.site.register(Section)
--- wiki/models.py
+++ wiki/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+class Section(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __unicode__(self):
+        return self.name
 
 class Author(models.Model):
     name = models.CharField(max_length=50)
@@ -12,6 +17,6 @@ class Page(models.Model):
     name = models.CharField(max_length=255, unique=True)
     content = models.TextField()
     author = models.ForeignKey('Author')
-    
+    section = models.ForeignKey('Section')
     def __unicode__(self):
         return self.name
