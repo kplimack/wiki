59c38f88f92819b5997eec6927c4c21870b0f3c0
Jake Plimack <jake.plimack@gmail.com>
2014-09-16 12:21:03 -0600

    disable django toolbar for now as its throwing errs

--- thewikiproject/settings.py
+++ thewikiproject/settings.py
@@ -110,7 +110,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'thewikiproject.urls'
